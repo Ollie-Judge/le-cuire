dc80f464582a096bbdb6914b24d68e9b92d570cc
Ollie-Judge <ollie.judge@hotmail.com>
2021-11-03 17:10:17 +0000

    fixed teh insert_one error

--- app.py
+++ app.py
@@ -33,7 +33,27 @@ def get_recipes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("This username already exists, try something different")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Thank You For Registering With Le Cuire")
     return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
