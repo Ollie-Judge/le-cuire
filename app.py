6737b0c90ee8a62a55d3b6cc63157caf7be6ef99
Ollie-Judge <ollie.judge@hotmail.com>
2021-11-08 21:30:23 +0000

    added recipe search functionality

--- app.py
+++ app.py
@@ -31,6 +31,13 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    recipes = mongo.db.recipes.find({"$text": {"$search": query}})
+    return render_template("recipes.html", recipes=recipes)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
